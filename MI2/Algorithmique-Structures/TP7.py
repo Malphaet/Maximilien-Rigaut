40c070e6956dbd6909e9a9a715c727d238f213b6
Maximilien Rigaut <max.rigaut@gmail.com>
2011-11-09 22:40:35 +0100

    Hanoi, fini

--- MI2/Algorithmique-Structures/TP7.py
+++ MI2/Algorithmique-Structures/TP7.py
@@ -8,10 +8,10 @@
 
 ##############################
 # Definition des Fonctions
-
 # - - - - - - - - - - - - - -
 # Fibonacci
 # - - - - - -
+
 def sub_fib(text):
 	res=''
 	for l in text:
@@ -32,41 +32,31 @@ def fibon(n):
 	if n==2: return "ab"
 	return fibon(n-1)+fibon(n-2)
 
-
 # - - - - - - - - - - - - - -
 # Hanoi
 # - - - - - -
 
 def hanoi_print(table):
-	taille=sum([len(i+[0]) for i in table])
-	res=['']*taille
-	i,mx=0,3
-	while i<mx:
-		j=0
-		for disque in uns(table[i]):
-			res[j]+=' '*(taille-disque)+'='*disque*2+' '*(taille-disque+4)
-			j+=1
-		i+=1
-	for line in uns(res): print line
-
-def uns(table):
-	res=[]
-	for i in table: res=[i]+res
-	return res
+	taille=sum([len(i) for i in table])
+	def segment(table,h):
+		if len(table)>h: return ' '*(taille-table[h])+"="*2*table[h]+' '*(taille-table[h])
+		return ' '*2*taille
+	for h in range(taille-1,-1,-1): print segment(table[0], h), segment(table[1], h), segment(table[2], h)
 
 def saut(i,j,t):
-	pass
+	hanoi(len(t[i]),i,j,[x for x in range(3) if x not in [i,j]][0],t)
 	
 def hanoi(n,i,j,k,t):
-	if n==1: t[i],t[j]=t[i][:-1],t[j]+[t[i][-1]]
-	hanoi(n-1,i,k,j,t)
-	hanoi(1,i,j,k,t)
-	hanoi(n-1,k,j,i,t)
-	hanoi_print(t)
+	if n==1: 
+		t[i],t[j]=t[i][:-1],t[j]+[t[i][-1]]
+		return hanoi_print(t)
+	hanoi(n-1,i,k,j,t), hanoi(1,i,j,k,t),hanoi(n-1,k,j,i,t)
 ##############################
 # Corps du programme
 
 ##for i in range(13): print fibon(i)
 
-table=[[1,2,3,4,5,6,7],[],[]]
-hanoi_print(table)
+table=[range(7,0,-1),[],[]]
+#hanoi_print(table)
+#hanoi(4,0,1,2,table)
+saut(0,1,table)
