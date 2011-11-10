9236a9484b472022fc76468c16331443d91ca523
Maximilien Rigaut <max.rigaut@gmail.com>
2011-11-10 12:44:57 +0100

    TP8, Over

--- MI2/Algorithmique-Structures/TP8.py
+++ MI2/Algorithmique-Structures/TP8.py
@@ -34,12 +34,25 @@ h_word=lambda w: h(char2oct(w))
 # Exo 3
 
 def anniversaire(n):
-	res,i,j,p=1.,365,365-n,1
+	res,i,j=1.,365,365-n
 	if n>355: return float('inf')
-	while i>0:
-		res=res*i/(j*(365**p))
-		i,j,n,p=i-1,max(j-1,1),n-1,max(min(n,1),0)
-		if i%30==0: print res,i,j,p
+	while j>0:
+		res=res*i/j
+		i,j=i-1,j-1
+	while n>0:
+		res=res*i/365
+		i,n=i-1,n-1
+	return 1-res
+
+def collision(n,m):
+	res,i,j=1.,m,m-n
+	if n>m: return float('inf')
+	while j>0:
+		res=res*i/j
+		i,j=i-1,j-1
+	while n>0:
+		res=res*i/m
+		i,n=i-1,n-1
 	return 1-res
 ##############################
 # Corps du programme
@@ -55,4 +68,5 @@ lang['maux']='woe'
 #print a,b
 #print h(a),h(b)
 
-print anniversaire(23)
+print anniversaire(23) # La date d'anniversaire n'est pas un bon hash pour une base de noms, 50% probs de collision (meme date d'anniv)
+print collision(12,24)
