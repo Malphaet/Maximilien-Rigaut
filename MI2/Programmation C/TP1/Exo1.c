a7ae8c5bbfaf96d10bbf5032a332e6e98fbb6ab4
Maximilien Rigaut <max.rigaut@gmail.com>
2012-01-16 18:02:37 +0100

    Programmation en C - TP1

---
+++ MI2/Programmation C/TP1/Exo1.c
@@ -0,0 +1,16 @@
+/*-----------------------------*/
+/* Copyleft Maximilien Rigaut  */
+/* TP1 - Exo1                  */
+/*-----------------------------*/
+
+
+#include <stdio.h>
+
+int main(void){
+	int a,b,c;
+	a=2;
+	b=3*a;
+	c=a+b;
+	printf("Bonjour: %d + %d = %d \n",a,b,c);
+	return 0;
+}
