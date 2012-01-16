829d33e1dddc194233efa14265870a032f4d956c
Maximilien Rigaut <max.rigaut@gmail.com>
2012-01-17 00:38:18 +0100

    Programmation C - TP1 Addded Exo 6 None compiled of course

--- MI2/Programmation C/TP1/Exo6.c
+++ MI2/Programmation C/TP1/Exo6.c
@@ -8,11 +8,25 @@
 
 int main(void){
 	int max,min,sum,nb,i;
-	i=0;
+	int start;
+	float moy;
+	i=0; sum=0;
+	start=1; /* Initialisation de Max et Min */
 	while (1){
 		printf("Entrez un nombre: ");
 		scanf("%d",&i);
 		if (i<0) break;
+		if (start) {
+			min=max=i;
+			start=0;
+		}
+		if (i<min) min=i;
+		if (i>max) max=i;
+		sum+=i;
+		nb++;
 	}
+	if (nb>0) moy=(float)sum/nb;
+	else moy=0;
+	printf("Max=%d, Min=%d, Somme=%d, Moyenne=%f\n",max,min,sum,moy);
 	return 0;
 }
