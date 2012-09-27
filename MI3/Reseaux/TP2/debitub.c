7d16a0236980e3e83d3e6be59b7fc49906689591
Malphaet <max.rigaut@gmail.com>
2012-09-27 21:38:16 +0200

    cours-reseaux : TP2 finished.
    Everything works fine, fixed odd connection problem.

--- MI3/Reseaux/TP2/debitub.c
+++ MI3/Reseaux/TP2/debitub.c
@@ -26,45 +26,79 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <bor-util.h>
 
 /* ========= Defines ==========*/
-
+void handler(int signal);
+unsigned long int readed=0;
+#define SFSG printf("%d: So far so good\n",p)
 /* =========== Main ===========*/
 
+
 int main(int argv,char ** kwargs){
+	/* Variable declaration */
 	int p;
 	int fd[2];
 	
 	int buffersize;
 	char *buffer;
 	int tmp=0;
-	int timer=0;
-	srand((unsigned int) time(NULL));
 
-	if (argv<2) fprintf(stderr,"buffersize: Incorrect arguments given.\n");
+	srand((unsigned int) time(NULL));
+	
+	/* Initialasing */
+	if (argv<2) fprintf(stderr,"Buffersize: Incorrect arguments given.\n");
 	buffersize=atoi(kwargs[1]);
 	buffer=malloc(sizeof(char)*(buffersize+2));
-	if (buffer==NULL) perror("buffer: Malloc error");
-	while (tmp<buffersize+2) *(buffer+tmp++)='a' + (char) rand()%42;
+	if (buffer==NULL) perror("Buffer: Malloc error");
+	while (tmp<buffersize+2) *(buffer+tmp++)=' ' + (char) rand()%50;
 	
-	if (pipe(fd)<0) perror("piping: Generation error");
-	if ((p=fork())<0) perror ("fork: Generation error");
+	/* Main program */
+	if (pipe(fd)<0) perror("Pipeing: Generation error");
+	if ((p=fork())<0) perror ("Fork: Generation error");
+	
+	bor_signal(SIGPIPE,handler,0);
+
+
+/*	alarm(1);*/
+	/* Child */
 	if (p==0){
+			bor_signal(SIGALRM,handler,0);
 		close(fd[1]);
-		printf("%s\n",buffer);
+		printf("Buffer generated, streaming in process.\n");
+			alarm(1);
 /*		printf("Each dot represents %d chars (%dkb).\n",1024*buffersize,buffersize*(int)sizeof(char));*/
-		while (read(fd[0],buffer,buffersize)>=0) 
-		if (++timer>1024) {
-			timer=0;
-/*			printf("Moo");*/
-			printf("%i",rand()%10);
-		}
+		while ((tmp=read(fd[0],buffer,buffersize))>=0) readed+=tmp;
 		exit(EXIT_SUCCESS);
 	}
+	/* Master */
+	
+	bor_signal(SIGCHLD,handler,0);
 	close(fd[0]);
+
 	while(write(fd[1],buffer,buffersize)>=0);
 	return 0;
 }
 
 /* ========= Functions ========*/
+void handler(int signal){
+	switch(signal){
+		case SIGPIPE:
+			printf("Was that it ?\n");
+			exit(EXIT_FAILURE);
+			break;
+		case SIGALRM:
+			printf("Actual speed : %.2f mb/s\n",((float)readed)/1048576);
+			readed=0;
+			alarm(1);
+			break;
+		case SIGCHLD:
+			printf("Child process closed : Shutting down !\n");
+			exit(EXIT_FAILURE);
+			break;
+		default:
+			printf("Signal unknown %d : Stopping now...\n",signal);
+			exit(EXIT_FAILURE);
+	}
 
+}
