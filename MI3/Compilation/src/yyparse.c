38ccc6b11296c95752120738f2c8c51994ed3f5f
Malphaet <max.rigaut@gmail.com>
2013-02-17 04:03:34 +0100

    Compilation - A slight improvement, let's see where it goes

--- MI3/Compilation/src/yyparse.c
+++ MI3/Compilation/src/yyparse.c
@@ -29,85 +29,93 @@
 
 /************* Rules */
 
-#define PLCC_SYNTAX_ERROR(expected,got,id) PLCC_ERROR("Syntax error : Expected %s. Found '%s' <%d>",expected,got,id);
-#define PLCC_GET(exp_id,exp) if ((uc=yylex())!=exp_id) PLCC_SYNTAX_ERROR(exp,yytext,uc);
+#define PLCC_SYNTAX_ERROR(expected) PLCC_ERROR("Syntax error : Expected %s found '%s' <%d>",expected,yytext,uc);
+//#define PLCC_GET(exp_id,exp) if ((uc=yylex())!=exp_id) PLCC_SYNTAX_ERROR(exp);
+#define PLCC_NEW  (uc=yylex())
 
 void Programme(){
-    /*
-    Program -> PROGRAM ID ';' Body '.'
-    */
-    char *s = "program";
-    markupOpen(s);
-    
-    PLCC_GET(SIDENT,"identifier");
-    markupLeaf("id",yytext);
-    PLCC_GET(';',"';'");
-    
-    Corps();
-    PLCC_GET('.',"'.'");
-    
-    
-    markupClose(s);
+	//Program -> PROGRAM ID ';' Body '.'
+	
+	char *s = "program";
+	markupOpen(s);
+	
+	if (PLCC_NEW!=SIDENT) PLCC_SYNTAX_ERROR("identifier");
+	markupLeaf("id",yytext);
+	
+	if (PLCC_NEW!=';') PLCC_SYNTAX_ERROR("';'");
+	PLCC_NEW; Corps();
+	
+	if (PLCC_NEW!='.') PLCC_SYNTAX_ERROR("'.'");
+	markupClose(s);
 }
 
 void Corps(){
-    char *s = "body";
-    markupOpen(s);
-    
-    PLCC_GET(SVAR,"var");
-    markupOpen("var");
-    
-    //ListeDeclVar();
-    
-    markupClose("var");
-    PLCC_GET(';',"';'");
-    
-    markupClose(s);
+	char *s = "body";
+	markupOpen(s);
+	
+	if (uc==SVAR){
+		markupOpen("var");
+		PLCC_NEW; ListeDeclVar();
+		if (PLCC_NEW!=';') PLCC_SYNTAX_ERROR("';'");
+		PLCC_NEW;
+		markupClose("var");
+	}
+	while (uc==SPROCEDURE){
+		//DeclProcFun();
+		//if (PLCC_NEW!=';') PLCC_SYNTAX_ERROR("';'");
+		PLCC_NEW;
+	}
+	//BlocInstr();
+	
+	markupClose(s);
 }
 
 void ListeDeclVar(){
-    
+	//while (uc==SID){
+		//while (uc==SID){
+		//}
+	//}
 }
 /*
-    Programme -> PROGRAM ID ';' Corps '.'
-  Corps ->[ VAR ListeDeclVar ';' ] { DeclProcFun ';' } BlocInstr
-  ListeDeclVar -> DeclVar{ ';' DeclVar }
-  DeclVar -> ID { ',' ID } ':' Type
-  Type -> INTEGER | BOOLEAN | ARRAY '[' NUMERAL '..' NUMERAL ']' OF Type
-  DeclProcFun -> DeclProcedure | DeclFunction 
-  DeclProcedure -> PROCEDURE ID [ '(' ListeDeclVar ')' ] ; Corps 
-  DeclFunction -> FUNCTION ID '(' ListeDeclVar ')' ':' Type ';' Corps 
-  
-  Instruction -> AffectInstr | AppelProcedure | IfInstr | WhileInstr | BlocInstr | Empty 
-  AffectInstr -> Variable AFFECT Expression 
-  AppelProcedure -> ID [ '(' ParameterList ')' ] 
-  AppelFunction -> ID '(' ListeParam ')'
-  ListeParam -> Expression [ ',' Expression ] 
-  IfInstr -> IF Expression THEN Instruction [ ELSE Instruction ] 
-  WhileInstr -> WHILE Expression DO Instruction 
-  BlocInstr -> BEGIN Instruction { ';' Instruction } END 
-  
-  Expression -> Simpleexpression [ Relation Simpleexpression ]
-  Relation -> '<' | '=' | '>' | INFEG | DIFF | SUPEG 
-  Simpleexpression -> Facteur [ OpAdd Simpleexpression ] 
-  OpAdd -> '+' | '-' | OR
-  Facteur -> [ RelationUnaire ] Predicat [ OpMult Facteur ] 
-  RelationUnaire -> '-' | NOT
-  OpMult -> '*' | DIV | MOD | AND
-  Predicat -> AppelFunction | NUMERAL | Variable | '(' Expression ')' 
-  Variable -> ID [ '[' Expression ']' ] 
+	+Programme -> PROGRAM ID ';' Corps '.'
+	Corps ->[ VAR ListeDeclVar ';' ] { DeclProcFun ';' } BlocInstr
+	ListeDeclVar -> DeclVar{ ';' DeclVar }
+DeclVar -> ID { ',' ID } ':' Type
+Type -> INTEGER | BOOLEAN | ARRAY '[' NUMERAL '..' NUMERAL ']' OF Type
+DeclProcFun -> DeclProcedure | DeclFunction 
+DeclProcedure -> PROCEDURE ID [ '(' ListeDeclVar ')' ] ; Corps 
+DeclFunction -> FUNCTION ID '(' ListeDeclVar ')' ':' Type ';' Corps 
+
+Instruction -> AffectInstr | AppelProcedure | IfInstr | WhileInstr | BlocInstr | Empty 
+AffectInstr -> Variable AFFECT Expression 
+AppelProcedure -> ID [ '(' ParameterList ')' ] 
+AppelFunction -> ID '(' ListeParam ')'
+ListeParam -> Expression [ ',' Expression ] 
+IfInstr -> IF Expression THEN Instruction [ ELSE Instruction ] 
+WhileInstr -> WHILE Expression DO Instruction 
+BlocInstr -> BEGIN Instruction { ';' Instruction } END 
+
+Expression -> Simpleexpression [ Relation Simpleexpression ]
+Relation -> '<' | '=' | '>' | INFEG | DIFF | SUPEG 
+Simpleexpression -> Facteur [ OpAdd Simpleexpression ] 
+OpAdd -> '+' | '-' | OR
+Facteur -> [ RelationUnaire ] Predicat [ OpMult Facteur ] 
+RelationUnaire -> '-' | NOT
+OpMult -> '*' | DIV | MOD | AND
+Predicat -> AppelFunction | NUMERAL | Variable | '(' Expression ')' 
+Variable -> ID [ '[' Expression ']' ] 
 */
 
 /********** Affichage */
 
 void markupOpen(char *s){
-    printf("<%s>\n",s);
+	printf("<%s>\n",s);
 }
 
 void markupClose(char *s){
-    printf("</%s>\n",s);
+	printf("</%s>\n",s);
 }
 
 void markupLeaf(char *s, char *val){
-    printf("<%s>%s</%s>\n",s , val, s);
+	printf("<%s>%s</%s>\n",s , val, s);
 }
